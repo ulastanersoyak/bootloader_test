2ab51dc9ff16549192f9f8accacadaada3cad4bd
ulastanersoyak <ulastanersoyak@hotmail.com>
2023-10-07 22:08:54 +0300

    format change

--- makefile
+++ makefile
@@ -1,17 +1,32 @@
-assembler := nasm
-assembler_flags := -f bin
-bootloader := ./src/bootloader/boot.asm
 boot_binary := ./bin/boot.bin
 virtual_machine := qemu-system-x86_64
+SRC_DIR := ./src
+BIN_DIR := ./bin
+BUILD_DIR := ./build
 
-all: ./bin
-	$(assembler) $(assembler_flags) $(bootloader) -o $(boot_binary)
+FILES := $(BIN_DIR)/os.bin
+BOOT_ASM_SRC := $(SRC_DIR)/bootloader/boot.asm
+KERNEL_ASM_SRC := $(SRC_DIR)/kernel.asm
+KERNEL_ASM_OBJ := $(BUILD_DIR)/kernel.asm.o
 
-qemu:
+all: $(FILES)
+	rm -rf $(BIN_DIR)/os.bin
+	dd if=$(boot_binary) >> $(FILES)
+
+$(boot_binary): $(BOOT_ASM_SRC) | $(BIN_DIR)
+	nasm -f bin $(BOOT_ASM_SRC) -o $(boot_binary)
+
+$(KERNEL_ASM_OBJ): $(KERNEL_ASM_SRC) | $(BUILD_DIR)
+	nasm -f elf -g $(KERNEL_ASM_SRC) -o $(KERNEL_ASM_OBJ)
+
+qemu: $(boot_binary)
 	$(virtual_machine) -hda $(boot_binary)
 
 clean:
-	rm -rf ./bin
+	rm -rf $(BIN_DIR)
 
-./bin:
-	mkdir -p ./bin
+$(BIN_DIR):
+	mkdir -p $(BIN_DIR)
+
+$(BUILD_DIR):
+	mkdir -p $(BUILD_DIR)
--- src/bootloader/boot.asm
+++ src/bootloader/boot.asm
@@ -51,8 +51,8 @@ step2:
     mov eax, cr0
     or eax, 0x1
     mov cr0, eax
-    jmp CODE_SEG:load32
-
+;    jmp CODE_SEG:load32
+    jmp $
 ; GDT
 gdt_start:
 gdt_null:
@@ -60,42 +60,27 @@ gdt_null:
     dd 0x0
 ; offset 0x8
 gdt_code:     ; CS SHOULD POINT TO THIS
-    dw 0xffff ; Segment limit first 0-15 bits
-    dw 0      ; Base first 0-15 bits
-    db 0      ; Base 16-23 bits
-    db 0x9a   ; Access byte
-    db 11001111b ; High 4 bit flags and the low 4 bit flags
-    db 0        ; Base 24-31 bits
+    dw 0xffff ; segment limit first 0-15 bits
+    dw 0      ; base first 0-15 bits
+    db 0      ; base 16-23 bits
+    db 0x9a   ; access byte
+    db 11001111b ; high 4 bit flags and the low 4 bit flags
+    db 0        ; base 24-31 bits
 
 ; offset 0x10
 gdt_data:      ; DS, SS, ES, FS, GS
-    dw 0xffff ; Segment limit first 0-15 bits
-    dw 0      ; Base first 0-15 bits
-    db 0      ; Base 16-23 bits
-    db 0x92   ; Access byte
-    db 11001111b ; High 4 bit flags and the low 4 bit flags
-    db 0        ; Base 24-31 bits
+    dw 0xffff ; segment limit first 0-15 bits
+    dw 0      ; base first 0-15 bits
+    db 0      ; base 16-23 bits
+    db 0x92   ; access byte
+    db 11001111b ; high 4 bit flags and the low 4 bit flags
+    db 0        ; base 24-31 bits
 
 gdt_end:
 
 gdt_descriptor:
     dw gdt_end - gdt_start-1
     dd gdt_start
-[BITS 32]
-load32:
-    mov ax, DATA_SEG
-    mov ds, ax
-    mov es, ax
-    mov fs, ax
-    mov gs, ax
-    mov ss, ax
-    mov ebp, 0x00200000
-    mov esp, ebp
-    in al, 0x92
-    ;https://wiki.osdev.org/A20_Line enabling a20 line.
-    or al, 2
-    out 0x92, al
-    jmp $
 times 510-($-$$) db 0;fills 510 bytes of data ($ -> current address $$ -> beginning of current section 
                     ;so $-$$ -> how far this in this section
 dw 0xAA55;bios recognizes the bootloader from the signatur of 0x55AA on last 2 bytes of the 512 byte block. it is written reverse
